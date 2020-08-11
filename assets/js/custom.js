67998508963899a71368bb80590b52e9d064359a
anggiyastiputri <putrianggiyasti@gmail.com>
2020-08-11 22:09:47 +0700

    add func add to cart

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,34 @@
-function setModalPorductDetail(shopCart) {
+$(document).ready(function(){
+    let countCart = 0;
+    console.log('local', JSON.parse(localStorage.getItem('shop_cart')))
+    if (localStorage.getItem('shop_cart')=== null) {
+        countCart = 0;
+    } else {
+        let shopCart = JSON.parse(localStorage.getItem('shop_cart'))
+        shopCart.map((el) => {
+            console.log(el)
+            countCart += el.productQty
+        })
+    }
+    document.getElementById("data-count").innerHTML= countCart
+});
+
+function setModalPorductDetail(shopCartTemp) {
+    var dataTemp = {}
+    var shopCart = shopCartTemp.reduce(function(data, el) {
+        var id = el.productId;
+        if (!dataTemp[id] && parseInt(el.productQty) > 0) {
+            dataTemp[id] = {
+            productId: el.productId,
+            productName: el.productName,
+            productPrice: el.productPrice,
+            productImg: el.productImg,
+            productQty: el.productQty,
+            }
+            data.push(dataTemp[id]);
+        }
+        return data;
+    }, [])
     if(shopCart.length) {
         document.getElementById("productDetailBody").innerHTML = ''
         let detailProduct = "";
@@ -10,11 +40,20 @@ function setModalPorductDetail(shopCart) {
             detailProduct += '<div class="col-md-6">'
             detailProduct += '<h5 class="mb-1 mt-2"><b>'+item.productName+'</b></h5>';
             detailProduct += '<p class="mb-2"> Rp. '+price.toLocaleString()+'</p>';
-            detailProduct += `<div class=form-row row">
-            <div class="form-group col-md-5 pl-0">
-                <div class="input-group">
-                    <span class="input-group-btn">
-                        <button type="button" class="btn btn-default btn-number btn-number-left" data-type="minus" data-field="quant-${i}[1]"><i class="fa fa-minus" aria-hidden="true"></i></button>
+            if (qty > 1) {
+                detailProduct += `<div class=form-row row">
+                <div class="form-group col-md-5 pl-0">
+                    <div class="input-group">
+                        <span class="input-group-btn">
+                            <button type="button" class="btn btn-default btn-number btn-number-left" data-type="minus" data-field="quant-${i}[1]"><i class="fa fa-minus" aria-hidden="true"></i></button>`
+            } else {
+                detailProduct += `<div class=form-row row">
+                <div class="form-group col-md-5 pl-0">
+                    <div class="input-group">
+                        <span class="input-group-btn">
+                            <button type="button" class="btn btn-default btn-number btn-number-left" disabled="disabled" data-type="minus" data-field="quant-${i}[1]"><i class="fa fa-minus" aria-hidden="true"></i></button>`
+            }
+            detailProduct += `
                     </span>
                     <input type="text" name="quant-${i}[1]" class="form-control input-number" value=${qty} min="1" max="1000" name="qtyProduct">
                     <span class="input-group-btn">
@@ -40,6 +79,7 @@ function setModalPorductDetail(shopCart) {
         document.getElementById("productDetailBody").innerHTML = '<div class="col-md-12"><h4 class="text-center">Empty cart.</h4></div>'
     }
 }
+
 function productDetail(productId, productName, price, productImg) {
     document.getElementById("productName").innerHTML=productName;
     document.getElementById("productPrice").innerHTML='Rp. ' +price.toLocaleString();
@@ -61,27 +101,12 @@ function productDetail(productId, productName, price, productImg) {
     });	
     localStorage.setItem('shop_cart',JSON.stringify(shopCart));
 
-    if (productName === 'Coffe 1') {
-      $('#detail-pouch-1').show()
-      $('#detail-pouch-2').hide()
-      $('#detail-sachet').hide()
-    } else if (productName === 'Coffe 2') {
-      $('#detail-pouch-1').hide()
-      $('#detail-pouch-2').show()
-      $('#detail-sachet').hide()
-    } else {
-      $('#detail-pouch-1').hide()
-      $('#detail-pouch-2').hide()
-      $('#detail-sachet').show()
-    }
     $('#productDetailModal').modal('show')
   }
 
 function addToCart() {
     var qty = $('#qtyProduct').val();
-    var countCart = document.getElementById("data-count").innerText;
-    var total = parseInt(countCart) + parseInt(qty)
-    document.getElementById("data-count").innerHTML=total;
+    let total = 0
     $('#summaryModal').modal('show')
     $('#productDetailModal').modal('hide')
 
@@ -99,6 +124,7 @@ function addToCart() {
     var shopCart = shopCartTemp.reduce(function(data, el) {
         var id = el.productId;
         if (!dataTemp[id]) {
+            total += el.productQty
             dataTemp[id] = {
             productId: el.productId,
             productName: el.productName,
@@ -111,11 +137,11 @@ function addToCart() {
         return data;
     }, [])
 
+    document.getElementById("data-count").innerHTML=total;
+
     localStorage.setItem("shop_cart", JSON.stringify(shopCart));  //put the object back
 
     setModalPorductDetail(shopCart)
-
-    // localStorage.setItem('daftar_nama',JSON.stringify(shopCart));
 }
 
 function clearCart() {
@@ -127,20 +153,68 @@ function removeProductFromCart(productId) {
     var shopCartTemp = JSON.parse(localStorage.shop_cart)
     const shopCart = shopCartTemp.filter(item => parseInt(item.productId) !== parseInt(productId))
     setModalPorductDetail(shopCart)
+    var totalCart = 0
+    shopCart.map((el) => {
+        totalCart += el.productQty
+    })
+    document.getElementById("data-count").innerHTML=totalCart;
     localStorage.setItem("shop_cart", JSON.stringify(shopCart));  //put the object back
 }
 
+function showCart() {
+    let shopCart;
+    if (localStorage.getItem('shop_cart' )=== null) {
+        shopCart = [];
+    } else {
+        shopCart = JSON.parse(localStorage.getItem('shop_cart'))
+    }
+    setModalPorductDetail(shopCart)
+    $('#summaryModal').modal('show')
+}
+
 $(document).on('click', '.btn-number', function(e){
-// $('.btn-number').click(function(e){
     e.preventDefault();
     
     fieldName = $(this).attr('data-field');
-    console.log('fieldName', fieldName)
     type      = $(this).attr('data-type');
     var input = $("input[name='"+fieldName+"']");
     var currentVal = parseInt(input.val());
     if (!isNaN(currentVal)) {
         if(type == 'minus') {
+            var countCart = document.getElementById("data-count").innerText
+            var total = parseInt(countCart) - 1
+            document.getElementById("data-count").innerHTML=total
+
+            var shopCartTemp = JSON.parse(localStorage.shop_cart)
+    var productId = $('#productId').val();
+
+    for (var i = 0; i < shopCartTemp.length; i++) {
+        if(parseInt(productId) === parseInt(shopCartTemp[i].productId)){  //look for match with name
+            shopCartTemp[i].productQty += parseInt(qty);  //add two
+            break;  //exit loop since you found the person
+        }
+     }
+
+    var dataTemp = {}
+    var shopCart = shopCartTemp.reduce(function(data, el) {
+        var id = el.productId;
+        if (!dataTemp[id]) {
+            total += el.productQty
+            dataTemp[id] = {
+            productId: el.productId,
+            productName: el.productName,
+            productPrice: el.productPrice,
+            productImg: el.productImg,
+            productQty: el.productQty,
+            }
+            data.push(dataTemp[id]);
+        }
+        return data;
+    }, [])
+
+    document.getElementById("data-count").innerHTML=total;
+
+    localStorage.setItem("shop_cart", JSON.stringify(shopCart));  //put the object back
             
             if(currentVal > input.attr('min')) {
                 input.val(currentVal - 1).change();
@@ -150,6 +224,9 @@ $(document).on('click', '.btn-number', function(e){
             }
 
         } else if(type == 'plus') {
+            var countCart = document.getElementById("data-count").innerText
+            var total = parseInt(countCart) + 1
+            document.getElementById("data-count").innerHTML=total
 
             if(currentVal < input.attr('max')) {
                 input.val(currentVal + 1).change();
@@ -165,7 +242,6 @@ $(document).on('click', '.btn-number', function(e){
 });
 
 $(document).on('focusin', '.input-number', function(){
-// $('.input-number').focusin(function(){
    $(this).data('oldValue', $(this).val());
 });
 
@@ -176,7 +252,6 @@ $(document).on('change', '.input-number', function(){
     valueCurrent = parseInt($(this).val());
     
     name = $(this).attr('name');
-    console.log('name', name)
     if(valueCurrent >= minValue) {
         $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
     } else {
@@ -194,7 +269,6 @@ $(document).on('change', '.input-number', function(){
 });
 
 $(document).on('keydown', '.input-number', function(e){
-// $(".input-number").keydown(function (e) {
     // Allow: backspace, delete, tab, escape, enter and .
     if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
             // Allow: Ctrl+A
